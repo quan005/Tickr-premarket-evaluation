36d8942ee658580e04f1cd26f1adf3f4191d295f
quan005 <ralphealfreeman@gmail.com>
2023-06-17 06:23:10 -0700

    printing thirty_minute_stock_frame to see data

--- pre_market.py
+++ pre_market.py
@@ -194,6 +194,8 @@ class Pre_Market:
                 data=thirty_minute_historical_prices['aggregated']
             )
 
+            print('thirty minute frame = ', thirty_minute_stock_frame.frame)
+
             # create thirty minute indicator object
             thirty_minute_indicator_client = Indicators(
                 price_data_frame=thirty_minute_stock_frame)
