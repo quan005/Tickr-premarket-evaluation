d05e5ec378cae60f133c06d6fc280efa6da53414
quan005 <ralphealfreeman@gmail.com>
2023-06-17 08:33:00 -0700

    test

--- stock_frame.py
+++ stock_frame.py
@@ -126,7 +126,7 @@ class StockFrame():
         pd.DataFrame -- A pandas dataframe.
         """
 
-        price_df = price_df.set_index(['symbol', 'datetime'])
+        price_df = price_df.set_index(['symbol', 'datetime'], append=TRUE)
 
         print('mullti columns', price_df.columns)
 
