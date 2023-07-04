22973bac2fa40fd0c213213f817d2b2df1102098
quan005 <ralphealfreeman@gmail.com>
2023-07-04 13:27:19 -0700

    test

--- indicators.py
+++ indicators.py
@@ -134,10 +134,10 @@ class Indicators():
 
         duplicate_dic = dict(price_dic)
         sorted_dic = dict(sorted(duplicate_dic.items(), key=lambda item: item[1]['total_count'], reverse=True)[:40])
-        print('sorted_dic: ', sorted_dic)
-        key_levels = [high, low]
-        for key in sorted_dic:
-            print('key', key)
+        # print('sorted_dic: ', sorted_dic)
+        # key_levels = [high, low]
+        # for key in sorted_dic:
+        #     print('key', key)
         # for idx, (key, value) in enumerate(sorted_dic.items()):
         #     price_avg = round(sum(p[0] for p in value['prices']) / len(value['prices']), 3)
         #     if idx < 20 or sum(p[1] for p in value['prices']) / len(value['prices']) >= complete_volume_avg:
