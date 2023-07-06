db2dd8d6a67077dc91cf6e79d9b4d90f5bae7ffd
quan005 <ralphealfreeman@gmail.com>
2023-07-06 13:23:21 -0700

    test

--- pre_market.py
+++ pre_market.py
@@ -25,7 +25,8 @@ class Pre_Market:
         self.BOOTSTRAP_SERVER = config.get('main', 'BOOTSTRAP_SERVER')
         self.SCHEMA_REGISTRY_URL = config.get('main', 'SCHEMA_REGISTRY_URL')
         self.TOKEN = None
-        self.small_watchlist = ['AAPL']
+        self.small_watchlist = ['AMD', 'NKE', 'AAL',
+                                'UAA', 'SPY', 'PFE', 'BAC', 'CCL', 'AAPL']
         self.medium_watchlist = ['AMD', 'NKE', 'AAL', 'UAA', 'SPY', 'PFE', 'BAC', 'CCL', 'AAPL',
                                  'MSFT', 'CRM', 'BABA', 'PYPL', 'DKNG', 'WMT', 'JPM', 'DIS', 'PBR', 'UAL', 'RIVN', 'FCX', 'MRO', 'MU']
         self.large_watchlist = ['AMD', 'NKE', 'AAL', 'UAA', 'SPY', 'PFE', 'BAC', 'CCL', 'AAPL', 'MSFT', 'CRM', 'BABA',
